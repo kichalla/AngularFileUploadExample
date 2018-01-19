b342944b31c3f5303134bcc31cb0799af2426b08
Kiran Challa <kichalla@microsoft.com>
2018-01-19 06:48:30 -0800

    updated

--- AngularCSRFExample/wwwroot/js/site.js
+++ AngularCSRFExample/wwwroot/js/site.js
@@ -8,55 +8,17 @@
     return User;
 }());
 
-var myApp = angular.module('myApp', ['ngCookies']);
-myApp.directive('fileModel', ['$parse', function ($parse) {
-    return {
-        restrict: 'A',
-        link: function (scope, element, attrs) {
-            var model = $parse(attrs.fileModel);
-            var modelSetter = model.assign;
+var myApp = angular.module('myApp', []);
 
-            element.bind('change', function () {
-                scope.$apply(function () {
-                    modelSetter(scope, element[0].files[0]);
-                });
-            });
-        }
-    };
-}]);
-
-myApp.service('userService', ['$http', 'tokenService', function ($http, tokenService) {
+myApp.service('userService', ['$http', 'loginService', function ($http, loginService) {
     this.createUser = function (user) {
-        var fd = new FormData();
-        fd.append('name', user.name);
-        fd.append('age', user.age);
-        fd.append('zipcode', user.zipcode);
-        fd.append('curriculumVitae', user.curriculumVitae);
-
-        return $http.post('/file/upload', fd, {
-            transformRequest: angular.identity,
-            headers: {
-                'Content-Type': undefined
-            }
-        });
-    }
-
-    this.createUser1 = function (user) {
         return $http.post('/home/create', user);
     };
 }]);
 
-myApp.service('tokenService', ['$http', function ($http) {
-    this.createToken = function () {
-        return $http.get('/token/create');
-    };
-
-    this.createCSRFToken = function () {
-        return $http.get(
-            '/token/createCSRFToken',
-            {
-                headers: { 'Authorization': ' Bearer ' + this.Token.token }
-            });
+myApp.service('loginService', ['$http', function ($http) {
+    this.login = function () {
+        return $http.post('/auth/login', {username: 'foo', password: 'bar'});
     };
 }]);
 
@@ -81,62 +43,24 @@ myApp.controller('myCtrl', ['$scope', 'userService', function ($scope, userServi
                 $scope.showResponseData = false;
             });
     };
-
-    $scope.createUser1 = function () {
-        $scope.showStatus = false;
-        $scope.showResponseData = false;
-
-        var user = new User($scope.name, $scope.age, $scope.zipcode, $scope.curriculumVitae)
-
-        userService.createUser1(user).then(function (response) { // success
-            if (response.status == 200) {
-                $scope.statusMessage = "User created sucessfully.";
-                $scope.responseData = response.data;
-                $scope.showStatus = true;
-                $scope.showResponseData = true;
-            }
-        },
-            function (response) { // failure
-                $scope.statusMessage = "User creation failed with status code: " + response.status;
-                $scope.showStatus = true;
-                $scope.showResponseData = false;
-            });
-    };
 }]);
 
-myApp.controller('tokenCtrl', ['$scope', 'tokenService', function ($scope, tokenService, $cookies) {
-    $scope.createToken = function () {
+myApp.controller('authCtrl', ['$scope', 'loginService', function ($scope, loginService) {
+    $scope.login = function () {
         $scope.showStatus = false;
         $scope.showResponseData = false;
 
-        tokenService.createToken().then(function (response) { // success
+        loginService.login().then(function (response) { // success
             if (response.status == 200) {
-                $scope.statusMessage = "Token created sucessfully.";
-                $scope.responseData = response.data;
-                tokenService.Token = response.data;
-                $scope.showStatus = true;
-                $scope.showResponseData = true;
-            }
-        },
-            function (response) { // failure
-                $scope.statusMessage = "Token creation failed with status code: " + response.status;
+                $scope.statusMessage = "Logged into application sucessfully.";
                 $scope.showStatus = true;
                 $scope.showResponseData = false;
-            });
-    };
-    $scope.createCSRFToken = function () {
-        $scope.showStatus = false;
-        $scope.showResponseData = false;
-
-        tokenService.createCSRFToken().then(function (response) { // success
-            if (response.status == 200) {
-                $scope.statusMessage = "CSRF Token created sucessfully.";
-                $scope.showStatus = true;
             }
         },
             function (response) { // failure
-                $scope.statusMessage = "CSRF Token creation failed with status code: " + response.status;
+                $scope.statusMessage = "Login failed with status code: " + response.status;
                 $scope.showStatus = true;
+                $scope.showResponseData = false;
             });
     };
 }]);
