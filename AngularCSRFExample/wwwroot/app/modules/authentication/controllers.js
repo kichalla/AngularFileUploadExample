da1479ee02c218d481a3df13f173c80136e77ebe
Kiran Challa <kichalla@microsoft.com>
2018-01-19 15:12:58 -0800

    Added Angular modules

---
+++ AngularCSRFExample/wwwroot/app/modules/authentication/controllers.js
@@ -0,0 +1,35 @@
+﻿'use strict';
+
+angular.module('Authentication')
+
+    .controller('LoginController',
+    ['$scope', '$rootScope', '$location', 'AuthenticationService',
+        function ($scope, $rootScope, $location, AuthenticationService) {
+            // reset login status
+            AuthenticationService.ClearCredentials();
+
+            $scope.login = function () {
+                $scope.dataLoading = true;
+                AuthenticationService.Login($scope.email, $scope.password, function (response) {
+                    if (response.status == 200) {
+                        AuthenticationService.SetCredentials($scope.email);
+                        $location.path('/');
+                    } else {
+                        $scope.error = response.message;
+                        $scope.dataLoading = false;
+                    }
+                });
+            };
+
+            $scope.logout = function () {
+                AuthenticationService.Logout(function (response) {
+                    if (response.status == 200) {
+                        AuthenticationService.ClearCredentials();
+                        $location.path('/login');
+                    } else {
+                        $scope.error = response.message;
+                        $scope.dataLoading = false;
+                    }
+                });
+            }
+        }]);
---
+++ AngularCSRFExample/wwwroot/app/modules/authentication/services.js
@@ -0,0 +1,40 @@
+﻿'use strict';
+
+angular.module('Authentication')
+
+    .factory('AuthenticationService', ['$http', '$cookieStore', '$rootScope', '$timeout',
+        function ($http, $cookieStore, $rootScope, $timeout) {
+            var service = {};
+
+            service.Login = function (email, password, callback) {
+                return $http
+                    .post('/auth/login', { email: email, password: password })
+                    .then(function (response) {
+                        callback(response);
+                    });
+            };
+
+            service.Logout = function (callback) {
+                return $http
+                    .get('/auth/logout')
+                    .then(function (response) {
+                        callback(response);
+                    });
+            };
+
+            service.SetCredentials = function (email) {
+                $rootScope.globals = {
+                    currentUser: {
+                        email: email
+                    }
+                };
+                $cookieStore.put('globals', $rootScope.globals);
+            }
+
+            service.ClearCredentials = function () {
+                $rootScope.globals = {};
+                $cookieStore.remove('globals');
+            };
+
+            return service;
+        }]);
