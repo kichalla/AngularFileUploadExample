efc8773530a7c2aebe4e5d7cb40082b44d90cfc5
Kiran Challa <kichalla@microsoft.com>
2018-01-26 15:23:16 -0800

    updated

--- AngularCSRFExample/Controllers/AuthController.cs
+++ AngularCSRFExample/Controllers/AuthController.cs
@@ -22,15 +22,23 @@ namespace AngularCSRFExample.Controllers
                 return BadRequest(ModelState);
             }
 
-            var claims = new List<Claim>();
-            claims.Add(new Claim(ClaimTypes.Name, "kichalla", ClaimValueTypes.String, "http://test.com"));
-            var userIdentity = new ClaimsIdentity("kichalla-identity");
-            userIdentity.AddClaims(claims);
-            var userPrincipal = new ClaimsPrincipal(userIdentity);
+            if (loginModel.Email.ToLowerInvariant() == "foo@bar.com"
+                && loginModel.Password == "test")
+            {
+                var claims = new List<Claim>();
+                claims.Add(new Claim(ClaimTypes.Name, "foo", ClaimValueTypes.String, "http://foo.com"));
+                claims.Add(new Claim(ClaimTypes.Email, "foo@bar.com", ClaimValueTypes.String, "http://foo.com"));
+                var userIdentity = new ClaimsIdentity("foo-identity");
+                userIdentity.AddClaims(claims);
+                var userPrincipal = new ClaimsPrincipal(userIdentity);
 
-            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, userPrincipal);
+                // This will set the cookie(having this logged in user's claims) in the outgoing response
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, userPrincipal);
 
-            return RedirectToAction(nameof(RefreshAntiforgeryRequestToken));
+                return RedirectToAction(nameof(RefreshAntiforgeryRequestToken));
+            }
+
+            return Unauthorized();
         }
 
         [HttpGet]
@@ -41,18 +49,27 @@ namespace AngularCSRFExample.Controllers
             HttpContext.Response.Cookies.Append(
                 "XSRF-TOKEN",
                 tokens.RequestToken,
-                new CookieOptions() { HttpOnly = false, SameSite = SameSiteMode.Lax });
+                new CookieOptions()
+                {
+                    HttpOnly = false,
+                    SameSite = SameSiteMode.Lax,
+                    Secure = true
+                });
         }
 
         [HttpGet]
         public async Task<IActionResult> Logout([FromServices] IOptions<AntiforgeryOptions> antiforgeryOptions)
         {
+            // Remove the session cookie from the outgoing response
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
 
+            // Remove the Antiforgery token cookies associated with the logged out user
             var antiforgeryCookieName = antiforgeryOptions.Value.Cookie.Name;
             HttpContext.Response.Cookies.Delete(antiforgeryCookieName);
             HttpContext.Response.Cookies.Delete("XSRF-TOKEN");
 
+            // Refresh the antifrogery token again so that the login form can use antiforgery tokens which are not
+            // associated with any identity.
             return Redirect(nameof(RefreshAntiforgeryRequestToken));
         }
     }
